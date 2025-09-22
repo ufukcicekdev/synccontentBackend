8c627e21a8bf3484c8a14442cad9230813a3396d
EAGLEEARTH <ufukcicek199@gmail.com>
2025-09-22 15:11:52 +0300

    push

--- socialsync/settings.py
+++ socialsync/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-key-change-in-pro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,synccontents.com,www.synccontents.com', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,synccontents.com,www.synccontents.com,web-production-3a29.up.railway.app', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
