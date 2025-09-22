76f35c98404144ad1020790b328f3c7a0ca0c1df
EAGLEEARTH <ufukcicek199@gmail.com>
2025-09-22 10:45:37 +0300

    push

--- socialsync/settings.py
+++ socialsync/settings.py
@@ -202,7 +202,7 @@ SIMPLE_JWT = {
 # CORS settings
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS', 
-    default='http://localhost:3000,http://127.0.0.1:3000,http://localhost:3001,http://127.0.0.1:3001,http://localhost:3002,http://127.0.0.1:3002',
+    default='http://localhost:3000,http://127.0.0.1:3000,http://localhost:3001,http://127.0.0.1:3001,http://localhost:3002,http://127.0.0.1:3002,https://web-production-3a29.up.railway.app',
     cast=lambda v: [s.strip() for s in v.split(',')]
 )
 
